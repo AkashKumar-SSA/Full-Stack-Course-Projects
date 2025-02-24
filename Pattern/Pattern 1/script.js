bda8101a6e2aac2895d2e9dfc7516641c184d113
Akash <akashkr6060@gmail.com>
2025-02-24 20:38:13 +0530

    .

--- Pattern/Pattern 1/script.js
+++ Pattern/Pattern 1/script.js
@@ -1,9 +1,16 @@
 function printTriangle(n) {
+ 
     for (let i = 1; i <= n; i++) {
-        let row = '';
-        for (let j = 1; j <= i; j++) {
-            row += '*';
+        let space = " ";
+        let star = "";
+        let row = "";
+        for (let j=0; j < i; j+=1){
+            row += space;
         }
+        for (let j = 1; j <= n-i; j++) {
+            star += '*';
+        }
+        row = star + space;
         console.log(row);
     }
 }
