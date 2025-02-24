3549d046761ff1dac0059f1826eba39c86f234d9
Akash <akashkr6060@gmail.com>
2025-02-24 19:55:39 +0530

    created new Pattern folder for the Javascript

---
+++ Pattern/Pattern 1/script.js
@@ -0,0 +1,12 @@
+function printTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += '*';
+        }
+        console.log(row);
+    }
+}
+
+// Call the function with the desired number of rows
+printTriangle(5);
