f2446ad10881df4d3968be5f403af86773681e38
Akash <akashkr6060@gmail.com>
2025-03-03 11:57:04 +0530

    Assignments done

---
+++ assignment/array/largestSortest.js
@@ -0,0 +1,10 @@
+// Find Largest and Smallest
+
+// Create an array of 5 numbers.
+// Write code to find the largest and smallest numbers in the array without using built-in functions.
+// Print both values.
+
+let numbers = [5, 10, 8, 3, 7];
+
+let largest = numbers[0];
+let smallest = numbers[0];d
