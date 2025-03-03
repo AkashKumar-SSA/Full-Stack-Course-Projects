127ac953d2a1d4f429ef973930a09cc8ac4b17bc
Akash <akashkr6060@gmail.com>
2025-03-03 13:36:40 +0530

    working on the assignments

---
+++ assignment/objectAndArrays/movielist.js
@@ -0,0 +1,20 @@
+// Movie List
+
+// Create an array of objects, each containing movieName, rating, and year.
+// Write code to filter movies released after 2015 and sort them by rating in descending order.
+
+const movies = [
+    {movieName:'movie1',rating: 4, year: 2016},
+    {movieName:'movie2',rating: 3, year: 2020},
+    {movieName:'movie3',rating: 5, year: 2014},
+    {movieName:'movie4',rating: 1, year: 2015},
+    {movieName:'movie5',rating: 3, year: 2000}
+];
+
+const newMovieList = {};
+for (let i = 0; i < movies.length; i++) {
+    if (movies[i].year >2015){
+        newMovieList.pu(movies[i]);
+    }
+}
+console.log(newMovieList);
